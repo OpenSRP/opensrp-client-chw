04b626210da22f3e0c158a6af48e33569d30bec3
qaziabubakar-vd <qaziabubakar47@gmail.com>
2021-07-21 03:36:55 +0500

    - Birth registration question added to birthnotification form. Also fixed other bugs

--- opensrp-chw/src/main/java/org/smartregister/chw/util/Utils.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/util/Utils.java
@@ -133,4 +133,14 @@ public class Utils extends org.smartregister.chw.core.utils.Utils {
         }
     }
 
+    public static String getClientName(String firstName, String middleName) {
+        String trimFirstName = firstName.trim();
+        String trimMiddleName = middleName.trim();
+        if (ChwApplication.getApplicationFlavor().hasSurname()) {
+            return getName(trimFirstName, trimMiddleName);
+        } else {
+            return Utils.getName(trimFirstName, trimMiddleName);
+        }
+    }
+
 }
